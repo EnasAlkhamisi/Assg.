0db4e0e17944dd5a3e7502a7d7baebf0ebde63ae
EnasAlkhamisi <Alkhamisienas@gmail.com>
2024-03-21 11:06:24 +0300

    assignmnt 3/5

---
+++ src/test/java/junit/Assignment3week5.java
@@ -0,0 +1,46 @@
+package junit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Assignment3week5 {
+    protected WebDriver driver;
+    protected Wait<WebDriver> wait;
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @AfterClass
+    public void afterMethod() {
+        driver.quit();
+    }
+
+    @Test
+    public void testInvalidPassword() {
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        // Enter username and incorrect password
+        driver.findElement(By.id("username")).sendKeys("student");
+        driver.findElement(By.id("password")).sendKeys("incorrectPassword");
+
+        // Click on login button
+        driver.findElement(By.id("submit")).click();
+
+        // Soft assert error message
+        String errorMessage = driver.findElement(By.id("error")).getText();
+        Assert.assertTrue(errorMessage.contains("Your password is invalid!"), "Error message validation failed");
+
+
+    }
+}
