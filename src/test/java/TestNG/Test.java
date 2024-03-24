d6872e872a341915d59f1cddf0165e351f750f1a
EnasAlkhamisi <Alkhamisienas@gmail.com>
2024-03-24 14:45:43 +0300

    updated

--- src/test/java/TestNG/Test.java
+++ src/test/java/TestNG/Test.java
@@ -1,45 +1,96 @@
 package TestNG;
 import engine.ActionsBot;
+//import engine.CustomListener;
+//import engine.PropertiesReader;
+import io.qameta.allure.Step;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.config.Configurator;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+//import org.json.simple.JSONObject;
+//import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
 public abstract class Test {
     protected WebDriver driver;
     protected Wait<WebDriver> wait;
-    public static Logger logger;
-    public ActionsBot bot;
+    protected static Logger logger;
+    protected ActionsBot bot;
+//    protected static JSONObject testData;
+//    protected JSONObject testCaseData;
 
-    @BeforeAll
-    public static void beforeAll(){
+    @Step("Initializing test data and properties")
+    @BeforeClass
+    public static void globalSetup() throws IOException, ParseException {
         Configurator.initialize(null, "src/main/resources/properties/log4j2.properties");
         logger = LogManager.getLogger(Test.class.getName());
+//        testData =  (JSONObject) new JSONParser().parse( new FileReader("src/test/resources/testData/sample.json", StandardCharsets.UTF_8) );
+//        PropertiesReader.readPropertyFile("src/main/resources/properties/configuration.properties");
     }
 
-    @BeforeEach
-    public void beforeEach(){
-        logger.info("Opening Chrome Browser");
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("start-maximized");
-        driver = new ChromeDriver(chromeOptions);
+//    @Step("Initializing target browser")
+//    @Parameters({ "target-browser" })
+//    @BeforeMethod
+//    public void browserInitialization(@Optional("chrome") String targetBrowser){
+//        targetBrowser = PropertiesReader.props.getProperty("targetBrowser");
+//        logger.info("Launching "+targetBrowser+" browser");
+//
+//        switch (targetBrowser){
+//            case "chrome" -> driver = new ChromeDriver();
+//            case "firefox" -> driver = new FirefoxDriver();
+//            case "safari" -> driver = new SafariDriver();
+//            case "edge" -> driver = new EdgeDriver();
+//        }
+//
+//        driver = new EventFiringDecorator(new CustomListener()).decorate(driver);
+//
+//        driver.manage().window().maximize();
+//
+//        logger.info("Configuring 5 second explicit wait");
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+//        bot = new ActionsBot(driver, wait, logger);
+//    }
+@BeforeMethod
 
-        logger.info("Configuring 5 second explicit wait");
-        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        bot = new ActionsBot(driver, wait, logger);
-    }
+public void beforeMethod(@Optional("chrome") String targetBrowser){
 
-    @AfterEach
-    public void afterEach(){
+
+
+    logger.info("Opening Chrome Browser");
+
+    ChromeOptions chromeOptions = new ChromeOptions();
+
+    chromeOptions.addArguments("start-maximized");
+
+    driver = new ChromeDriver(chromeOptions);
+
+
+
+    logger.info("Configuring 5 second explicit wait");
+
+    wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+    bot = new ActionsBot(driver, wait, logger);
+
+}
+    @Step("Terminating target browser")
+    @AfterMethod
+    public void browserTermination(){
         logger.info("Quitting Browser");
         driver.quit();
     }
